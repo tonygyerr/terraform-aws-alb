5ec465f5c9f6594b00671a3678921ae001b9cb99
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-09-20 15:01:33 -0400

    _outputs.tf

--- _outputs.tf
+++ _outputs.tf
@@ -1,24 +1,24 @@
 output "route53_zone_zone_id" {
   description = "Zone ID of Route53 zone"
-  value       = aws_route53_zone[count.index].this.zone_id
+  value       = aws_route53_zone.this.zone_id
 }
 
 output "route53_zone_name_servers" {
   description = "Name servers of Route53 zone"
-  value       = aws_route53_zone[count.index].this.name_servers
+  value       = aws_route53_zone.this.name_servers
 }
 
 output "route53_zone_name" {
   description = "Name of Route53 zone"
-  value       = aws_route53_zone.this[count.index].name
+  value       = aws_route53_zone.this.name
 }
 
 output "route53_record_name" {
   description = "The name of the record"
-  value       = aws_route53_record.this[count.index].name
+  value       = aws_route53_record.this.name
 }
 
 output "route53_record_fqdn" {
   description = "FQDN built using the zone domain and name"
-  value       = aws_route53_record.this[count.index].fqdn
+  value       = aws_route53_record.this.fqdn
 }
