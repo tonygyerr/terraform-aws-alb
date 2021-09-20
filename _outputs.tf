ea8a645782804ada075fbe34748450b6b7c2ba70
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-09-20 14:18:59 -0400

    _outputs.tf

--- _outputs.tf
+++ _outputs.tf
@@ -15,10 +15,10 @@ output "route53_zone_name" {
 
 output "route53_record_name" {
   description = "The name of the record"
-  value       = aws_route53_record.this.name
+  value       = aws_route53_record.this[count.index].name
 }
 
 output "route53_record_fqdn" {
   description = "FQDN built using the zone domain and name"
-  value       = aws_route53_record.this.fqdn
+  value       = aws_route53_record.this[count.index].fqdn
 }
