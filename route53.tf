be18c86c276b6e8703677f2e68c00c93cc504ce8
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-09-20 22:12:59 -0400

    route53.tf

--- route53.tf
+++ route53.tf
@@ -14,7 +14,7 @@ resource "aws_route53_record" "this" {
   zone_id   = aws_route53_zone.this.zone_id #data.aws_route53_zone.app.zone_id
   name      = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
   type      = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  records   = [aws_acm_certificate.acm_certificate.domain_validation_options.0.resource_record_value]
+  records   = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
   ttl       = "60"
 
   alias {
