c4da98bb2d4be6bc689b057a9be77f8e109c9ef2
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-09-20 20:20:45 -0400

    route53.tf

--- route53.tf
+++ route53.tf
@@ -9,12 +9,13 @@ resource "aws_route53_zone" "this" {
 
 resource "aws_route53_record" "this" {
   # count      = var.load_balancer_type == "application" ? 0 : 1
-  depends_on = [aws_acm_certificate.cert]
+  allow_overwrite = true
+  # depends_on = [aws_acm_certificate.cert]
   zone_id   = aws_route53_zone.this.zone_id #data.aws_route53_zone.app.zone_id
   name      = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
   type      = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  # records   = [aws_acm_certificate.acm_certificate.domain_validation_options.0.resource_record_value")]
-  # ttl       = "60"
+  records   = [aws_acm_certificate.acm_certificate.domain_validation_options.0.resource_record_value]
+  ttl       = "60"
 
   alias {
     name    = aws_lb.public.dns_name
