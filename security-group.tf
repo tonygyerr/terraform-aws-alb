ecc53d91d0c9556575f05b0a25006ede3d8ebe07
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-10-30 18:18:10 -0400

    security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,27 +1,27 @@
 resource "aws_security_group" "app" {
   name        = "${var.app_name}-alb-sg"
   description = "Application Security Group"
-  vpc_id      = var.vpc_config.vpc_id 
+  vpc_id      = var.vpc_config.cidr 
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_config.vpc_id]
+    cidr_blocks = [var.vpc_config.cidr]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_config.vpc_id]
+    cidr_blocks = [var.vpc_config.cidr]
   }
 
   ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_config.vpc_id]
+    cidr_blocks = [var.vpc_config.cidr]
   }
 
   egress {
