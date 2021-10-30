42a768aca421b76f6187bd5ecb9d8dbcf6d0082d
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-10-30 11:54:52 -0400

    security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -3,62 +3,70 @@ resource "aws_security_group" "app" {
   description = "Application Security Group"
   vpc_id      = var.vpc_config.vpc_id 
 
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = 6
+      description = ""
+      cidr_blocks = var.vpc_config.vpc_id
+    },
+    {
+      from_port   = 8000
+      to_port     = 8000
+      protocol    = 6
+      description = ""
+      cidr_blocks = var.vpc_config.vpc_id
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = 6
+      description = ""
+      cidr_blocks = var.vpc_config.vpc_id
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = 6
+      description = ""
+      cidr_blocks = var.vpc_config.vpc_id
+    },
+  ]
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_config.cidr]
-  }
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 65535
+      protocol    = -1
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_config.cidr]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_config.cidr]
-  }
-
-  ingress {
-    from_port   = 8000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_config.cidr]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = 6
-    description = ""
-    cidr_blocks = [var.vpc_config.cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = 0
-    description = ""
-    cidr_blocks = [var.vpc_config.open_cidr]
-  }
+resource "aws_security_group_rule" "http"{
+  type = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  security_group_id = aws_security_group.app.id
+  source_security_group_id = aws_security_group.app.id
 }
 
 resource "aws_security_group_rule" "app"{
   type = "ingress"
-  from_port   = 0 
-  to_port     = 65535 
+  from_port   = 8000
+  to_port     = 8000
+  protocol    = "tcp"
+  security_group_id = aws_security_group.app.id
+  source_security_group_id = aws_security_group.app.id
+}
+
+resource "aws_security_group_rule" "https"{
+  type = "ingress"
+  from_port   = 443
+  to_port     = 443 
   protocol    = "tcp"
   security_group_id = aws_security_group.app.id
   source_security_group_id = aws_security_group.app.id
