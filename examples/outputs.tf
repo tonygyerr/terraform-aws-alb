3c833a485d15fb77ffcc261e1834a0f25af59ff7
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-11-21 09:05:22 -0500

    outputs.tf

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -13,31 +13,31 @@
 # #   value       = "${module.asg.asg_autoscaling_group_name}"
 # # }
 
-output "route53_zone_zone_id" {
-  description = "Zone ID of Route53 zone"
-  value       = module.alb.route53_zone_zone_id
-}
+# output "route53_zone_zone_id" {
+#   description = "Zone ID of Route53 zone"
+#   value       = module.alb.route53_zone_zone_id
+# }
 
-output "route53_zone_name_servers" {
-  description = "Name servers of Route53 zone"
-  value       = module.alb.route53_zone_name_servers
-}
+# output "route53_zone_name_servers" {
+#   description = "Name servers of Route53 zone"
+#   value       = module.alb.route53_zone_name_servers
+# }
 
-output "route53_zone_name" {
-  description = "Name of Route53 zone"
-  value       = module.alb.route53_zone_name
-}
+# output "route53_zone_name" {
+#   description = "Name of Route53 zone"
+#   value       = module.alb.route53_zone_name
+# }
 
-# records
-output "route53_record_name" {
-  description = "The name of the record"
-  value       = module.alb.route53_record_name
-}
+# # records
+# output "route53_record_name" {
+#   description = "The name of the record"
+#   value       = module.alb.route53_record_name
+# }
 
-output "route53_record_fqdn" {
-  description = "FQDN built using the zone domain and name"
-  value       = module.alb.route53_record_fqdn
-}
+# output "route53_record_fqdn" {
+#   description = "FQDN built using the zone domain and name"
+#   value       = module.alb.route53_record_fqdn
+# }
 
 output "alb_target_group_https_arn" {
   description = "Alb Target Group ARN"
