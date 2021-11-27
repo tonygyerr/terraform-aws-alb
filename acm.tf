125bbe9558c3ab149319f5f1a336b5053d6656b0
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-11-27 18:25:20 -0500

    acm.tf

--- acm.tf
+++ acm.tf
@@ -1,8 +1,8 @@
-resource "aws_acm_certificate" "cert" {
-  private_key       = var.tls_pem_filename #data.template_file.ppk.rendered 
-  certificate_body  = var.tls_ppk_filename #data.template_file.pem.rendered
-  tags = merge(map("Name", "${var.app_name}-cert"), var.tags)
-}
+# resource "aws_acm_certificate" "cert" {
+#   private_key       = var.tls_pem_filename #data.template_file.ppk.rendered 
+#   certificate_body  = var.tls_ppk_filename #data.template_file.pem.rendered
+#   tags = merge(map("Name", "${var.app_name}-cert"), var.tags)
+# }
 
 # resource "aws_acm_certificate_validation" "cert" {
 #   certificate_arn         = aws_acm_certificate.cert.arn
